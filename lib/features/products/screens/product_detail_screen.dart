aab2df2f77e1c4e864fbb30dd75ab84454356dc0
Uvais-Mohammad <uvaiskc55@gmail.com>
2023-12-29 18:19:56 +0530

    Add ProductDetailScreen to display product details

---
+++ lib/features/products/screens/product_detail_screen.dart
@@ -0,0 +1,59 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flipkart_clone/features/products/models/product.dart';
+import 'package:flutter/material.dart';
+
+class ProductDetailScreen extends StatefulWidget {
+  const ProductDetailScreen({Key? key, required this.product})
+      : super(key: key);
+
+  final Product product;
+
+  @override
+  State<ProductDetailScreen> createState() => _ProductDetailScreenState();
+}
+
+class _ProductDetailScreenState extends State<ProductDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    Product product = widget.product;
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Hero(
+              tag: product.id,
+              child: CachedNetworkImage(
+                imageUrl: product.image,
+                height: MediaQuery.sizeOf(context).height * 0.45,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                product.title,
+                style: const TextStyle(fontSize: 20),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                product.description,
+                style: const TextStyle(fontSize: 16),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                '₹ ${product.price}',
+                style:
+                    const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
