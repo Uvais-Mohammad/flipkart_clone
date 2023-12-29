f454b42d599b4ea5cd81ce6299216924dec7ac42
Uvais-Mohammad <uvaiskc55@gmail.com>
2023-12-29 18:18:49 +0530

    Add category-related methods to IProductRepository

--- lib/features/products/repositories/i_product_repo.dart
+++ lib/features/products/repositories/i_product_repo.dart
@@ -2,4 +2,6 @@ import 'package:flipkart_clone/features/products/models/product.dart';
 
 abstract interface class IProductRepository {
   Future<List<Product>> getProducts();
+  Future<List<String>> getCategories();
+  Future<List<Product>> getProductsByCategory(String category);
 }
--- lib/features/products/repositories/product_repo.dart
+++ lib/features/products/repositories/product_repo.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flipkart_clone/features/products/models/product.dart';
 import 'package:flipkart_clone/features/products/repositories/i_product_repo.dart';
+import 'package:flipkart_clone/shared/config/apis.dart';
 import 'package:flipkart_clone/shared/services/rest_client/i_rest_client.dart';
 import 'package:flipkart_clone/shared/services/rest_client/rest_client.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -13,6 +16,25 @@ final class ProductRepository implements IProductRepository {
 
   ProductRepository({required IRestClient restClient})
       : _restClient = restClient;
+
   @override
-  Future<List<Product>> getProducts() {}
+  Future<List<Product>> getProducts() async {
+    final response = await _restClient.get(Apis.allProducts);
+    final List<dynamic> decodedResponse = jsonDecode(response.body);
+    return decodedResponse.map((e) => Product.fromJson(e)).toList();
+  }
+
+  @override
+  Future<List<String>> getCategories() async {
+    final response = await _restClient.get(Apis.categories);
+    final List<dynamic> decodedResponse = jsonDecode(response.body);
+    return decodedResponse.map((e) => e.toString()).toList();
+  }
+
+  @override
+  Future<List<Product>> getProductsByCategory(String category) async {
+    final response = await _restClient.get('${Apis.productsByCategory}$category');
+    final List<dynamic> decodedResponse = jsonDecode(response.body);
+    return decodedResponse.map((e) => Product.fromJson(e)).toList();
+  }
 }
