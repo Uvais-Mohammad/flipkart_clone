1f9eaedf09423397dc2e5416ac9c2589dbc69b06
Uvais-Mohammad <uvaiskc55@gmail.com>
2023-12-29 18:20:24 +0530

    Update API endpoints in Apis class

--- lib/shared/config/apis.dart
+++ lib/shared/config/apis.dart
@@ -1,4 +1,6 @@
 class Apis {
- static const String allProducts = "https://fakestoreapi.com/products";
+  static const String allProducts = "/products";
   static const String singleProduct = "https://fakestoreapi.com/products/";
+  static const String categories = "/products/categories";
+  static const String productsByCategory = "/products/category/";
 }
