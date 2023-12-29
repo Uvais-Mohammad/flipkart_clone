2ba7add2e6817b709cc49440bc146e509b1a4c4c
Uvais-Mohammad <uvaiskc55@gmail.com>
2023-12-29 18:19:37 +0530

    Update main.dart with MyApp import

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,11 @@
+import 'package:flipkart_clone/features/my_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   runApp(
     const ProviderScope(
-      child: MainApp(),
+      child: MyApp(),
     ),
   );
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
 }
